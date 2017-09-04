9ba09c55cdaf34c8b4ce0dc284906315d398fc50
J D Whitfield <James.D.Whitfield@dartmouth.edu>
2017-09-04 04:38:06 -0400

    Got the test_eigen working with both LAPACK and Eigen

--- SAuYF/fci_cpp/test_eigen.cpp
+++ SAuYF/fci_cpp/test_eigen.cpp
@@ -12,6 +12,8 @@
 #include<chrono>
 #include<algorithm>
 
+void
+reset_array(int dim, Eigen::MatrixXd matrix, double*& array);
 
 int 
 main()
@@ -39,14 +41,20 @@ main()
 	    for(int j=i; j < dim; j++) //a matrix stored in col-major form
 	    {
     		file >> matrix_col_major_upper[ctr];
+		if(debug)
+			cout << "read "<< ctr << " as " << matrix_col_major_upper[ctr] << " into (" << i << ","<<j<< ") with ";
 		matrix(i,j)=matrix_col_major_upper[ctr];
 		matrix(j,i)=matrix_col_major_upper[ctr];
+
 		if(i==j)
 			trace+=matrix_col_major_upper[ctr];
+
+		if(debug)
+	           cout << "\t" << ctr << " : " << (i*dim+j-i*(i+1)/2) << "\n";
     		ctr++;
 	    }
 
-    
+
     file.close();
     //done reading file
 
@@ -68,23 +76,89 @@ main()
     }
 
 
-     //<< "& idx:" << (i-1 + (j)*(j-1)/2) 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     //LAPACK
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     //diagonalization vars
     double* eigenvalues= new double[dim];  
     double* eigenvectors=new double[dim*dim];
+    int out;
 
     //timing variables
     std::chrono::duration<double> time_elapsed;
 
-    auto start_time = std::chrono::high_resolution_clock::now();
+    if(debug)
+    {
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    int out=LAPACKE_dspev(
+    reset_array(dim, matrix, matrix_col_major_upper);
+    out=LAPACKE_dspev(
             LAPACK_ROW_MAJOR, 
             'V',            // 'V' for eigvals and vects or 'N' for just eigvals
             'U',            // input is 'U' upper tri or 'L' lower tri
+            dim,            // matrix dimensions
+            matrix_col_major_upper, // pointer to matrix array
+            eigenvalues,    // on success, its filled with ordered eigvals 
+            eigenvectors,   // on success, ordered eigvects (if requested)
+            dim             // The leading dimension of the eigvect array >= 1
+                            // if JOBZ = 'V', LDZ >= max(1,N).
+             );
+ 
+
+    cout << "Row-major, U\n";
+    for (int j=0; j<dim; j++)
+	    cout << eigenvalues[j] << " ";
+    cout << "\n";
+    
+    reset_array(dim, matrix, matrix_col_major_upper);
+   
+  
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    out=LAPACKE_dspev(LAPACK_ROW_MAJOR, 'V',
+		    'L', dim,  matrix_col_major_upper, eigenvalues, eigenvectors, dim);
+ 
+    cout << "Row-major, L\n";
+    for (int j=0; j<dim; j++)
+	    cout << eigenvalues[j] << " ";
+    cout << "\n";
+
+    reset_array(dim, matrix, matrix_col_major_upper);
+  
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    out=LAPACKE_dspev(LAPACK_COL_MAJOR, 'V',
+		    'L', dim,  matrix_col_major_upper, eigenvalues, eigenvectors, dim);
+ 
+    cout << "Col-major, L\n";
+    for (int j=0; j<dim; j++)
+	    cout << eigenvalues[j] << " ";
+    cout << "\n";
+
+    reset_array(dim, matrix, matrix_col_major_upper);
+  
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    out=LAPACKE_dspev(LAPACK_COL_MAJOR, 'V',
+		    'U', dim,  matrix_col_major_upper, eigenvalues, eigenvectors, dim);
+ 
+    cout << "Col-major, U\n";
+    for (int j=0; j<dim; j++)
+	    cout << eigenvalues[j] << " ";
+    cout << "\n";
+
+    reset_array(dim, matrix, matrix_col_major_upper);
+  
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+    cout << "\n";
+    
+    }
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    out=LAPACKE_dspev(
+            LAPACK_COL_MAJOR, 
+            'V',            // 'V' for eigvals and vects or 'N' for just eigvals
+            'L',            // input is 'U' upper tri or 'L' lower tri
             dim,            // matrix dimensions
             matrix_col_major_upper, // pointer to matrix array
 	    /*
@@ -103,145 +177,31 @@ main()
              );
  
     time_elapsed = std::chrono::high_resolution_clock::now() - start_time;
-
-    cout << "Row-major, U\n";
-    for (int j=0; j<dim; j++)
-	    cout << eigenvalues[j] << " ";
-    cout << "\n";
-
-
-    
-     //convert to row-major upper triangular array from matrix
-     for(int i=0; i<dim; i++)
-	     for (int j=i;j<dim; j++)
-	     {
-
-	     /*Packed columnwise, then The j-th column of A is stored in the array
-	     * AP as follows: if UPLO = 'U', AP(i + (j-1)*j/2) =
-	     * A(i,j) for 1<=i<=j; if UPLO = 'L', AP(i + (j-1)*(2*n-j)/2) 
-	     * = A(i,j) for j<=i<=n.
-	     */
-		    auto idx=(i+j*(j+1)/2) ;
-		    matrix_col_major_upper[idx]=matrix(i,j);
-	     }
-
-
-    out=LAPACKE_dspev(
-            LAPACK_ROW_MAJOR, 
-            'V',            // 'V' for eigvals and vects or 'N' for just eigvals
-            'L',            // input is 'U' upper tri or 'L' lower tri
-            dim,            // matrix dimensions
-            matrix_col_major_upper, // pointer to matrix array
-	    /*
-	     * (input/output) DOUBLE PRECISION array, dimension
-	     * On entry, the upper or lower triangle of the sym-
-	     * metric matrix A, packed columnwise in a linear
-	     * array.  The j-th column of A is stored in the array
-	     * AP as follows: if UPLO = 'U', AP(i + (j-1)*j/2) =
-	     * A(i,j) for 1<=i<=j; if UPLO = 'L', AP(i + (j-1)*(2*n-j)/2) 
-	     * = A(i,j) for j<=i<=n.
-	     */
-            eigenvalues,    // on success, its filled with ordered eigvals 
-            eigenvectors,   // on success, ordered eigvects (if requested)
-            dim             // The leading dimension of the eigvect array >= 1
-                            // if JOBZ = 'V', LDZ >= max(1,N).
-             );
- 
-    cout << "Row-major, L\n";
-    for (int j=0; j<dim; j++)
-	    cout << eigenvalues[j] << " ";
-    cout << "\n";
-
-    
-     //convert to row-major upper triangular array from matrix
-     for(int i=0; i<dim; i++)
-	     for (int j=i;j<dim; j++)
-	     {
-		    auto idx=(i+j*(j+1)/2) ;
-		    matrix_col_major_upper[idx]=matrix(i,j);
-	     }
-
-
-    out=LAPACKE_dspev(
-            LAPACK_COL_MAJOR, 
-            'V',            // 'V' for eigvals and vects or 'N' for just eigvals
-            'L',            // input is 'U' upper tri or 'L' lower tri
-            dim,            // matrix dimensions
-            matrix_col_major_upper, // pointer to matrix array
-	    /*
-	     * (input/output) DOUBLE PRECISION array, dimension
-	     * On entry, the upper or lower triangle of the sym-
-	     * metric matrix A, packed columnwise in a linear
-	     * array.  The j-th column of A is stored in the array
-	     * AP as follows: if UPLO = 'U', AP(i + (j-1)*j/2) =
-	     * A(i,j) for 1<=i<=j; if UPLO = 'L', AP(i + (j-1)*(2*n-j)/2) 
-	     * = A(i,j) for j<=i<=n.
-	     */
-            eigenvalues,    // on success, its filled with ordered eigvals 
-            eigenvectors,   // on success, ordered eigvects (if requested)
-            dim             // The leading dimension of the eigvect array >= 1
-                            // if JOBZ = 'V', LDZ >= max(1,N).
-             );
- 
-    
-     //convert to row-major upper triangular array from matrix
-     for(int i=0; i<dim; i++)
-	     for (int j=i;j<dim; j++)
-	     {
-		    auto idx=(i+j*(j+1)/2) ;
-		    matrix_col_major_upper[idx]=matrix(i,j);
-	     }
-
-
-    cout << "col-major, L\n";
-    for (int j=0; j<dim; j++)
-	    cout << eigenvalues[j] << " ";
-    cout << "\n";
-
-    out=LAPACKE_dspev(
-            LAPACK_COL_MAJOR, 
-            'V',            // 'V' for eigvals and vects or 'N' for just eigvals
-            'U',            // input is 'U' upper tri or 'L' lower tri
-            dim,            // matrix dimensions
-            matrix_col_major_upper, // pointer to matrix array
-	    /*
-	     * (input/output) DOUBLE PRECISION array, dimension
-	     * On entry, the upper or lower triangle of the sym-
-	     * metric matrix A, packed columnwise in a linear
-	     * array.  The j-th column of A is stored in the array
-	     * AP as follows: if UPLO = 'U', AP(i + (j-1)*j/2) =
-	     * A(i,j) for 1<=i<=j; if UPLO = 'L', AP(i + (j-1)*(2*n-j)/2) 
-	     * = A(i,j) for j<=i<=n.
-	     */
-            eigenvalues,    // on success, its filled with ordered eigvals 
-            eigenvectors,   // on success, ordered eigvects (if requested)
-            dim             // The leading dimension of the eigvect array >= 1
-                            // if JOBZ = 'V', LDZ >= max(1,N).
-             );
- 
-    
-     //convert to row-major upper triangular array from matrix
-     for(int i=0; i<dim; i++)
-	     for (int j=i;j<dim; j++)
-	     {
-		    auto idx=(i+j*(j+1)/2) ;
-		    matrix_col_major_upper[idx]=matrix(i,j);
-	     }
-
-
-    cout << "col-major, U\n";
-    for (int j=0; j<dim*(dim+1)/2; j++)
-	    cout << matrix_col_major_upper[j];
-    cout << " -> ";
-    for (int j=0; j<dim; j++)
-	    cout << eigenvalues[j] << " ";
-    cout << "\n";
-
-
-
-
-
     std::cout << "LAPACK took : " << time_elapsed.count() << " ms " << std::endl;
+    
+    if(debug)
+    {
+    //convert to row-major upper triangular array from matrix
+    for(int i=0; i<dim; i++)
+	     for (int j=i;j<dim; j++)
+	     {
+		    auto idx=(i+j*(j+1)/2) ;
+		    matrix_col_major_upper[idx]=matrix(i,j);
+	     }
+
+
+    cout << "row-major, U\n";
+    //for (int j=0; j<dim*(dim+1)/2; j++)
+    //	    cout << matrix_col_major_upper[j];
+    //cout << " -> ";
+    for (int j=0; j<dim; j++)
+	    cout << eigenvalues[j] << " ";
+    cout << "\n";
+    }
+
+
+
+
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     //Eigen
@@ -277,54 +237,28 @@ main()
     cout << "eigen,lapack\n";
     for(int i=0; i<dim; i++)
     {
-	//    diff+=abs(evs_eigen[i]-evs_lapack[i]);
-	    cout<< i << " : " << evs_eigen[i]<< "," << evs_lapack[i] << endl;
+	    diff+=abs(evs_eigen[i]-evs_lapack[i]);
+   	    cout<< i << " : " << evs_eigen[i]<< "," << evs_lapack[i] << endl;
 	    trace_eigen+=evs_eigen[i];
 	    trace_lapack+=evs_lapack[i];
     }
 		   
-    //std::cout << "L_1 Discrepancy: " << diff << endl;
-    std::cout << "Trace Eigen :" << trace_eigen << endl;
-    std::cout << "Trace LAPACK: " << trace_lapack << endl;
+    std::cout << "L_1 Discrepancy: " << diff << endl;
 
     return 0;
 }
 
-
-double
-print_upper_triangular_matrix(int D, double *array)
+void
+reset_array(int dim, Eigen::MatrixXd matrix, double* &array)
 {
-    
-   using std::cout;
-
-   cout.precision(5);
-   cout << std::scientific;
-
-   /*
-     if UPLO = 'U', for 1<=i<=j
-     AP(i-1 + (j)*(j-1)/2) = A(i,j) 
-    */
-
-    int j=1,i=1;
-
-    for(int ctr=0; ctr<D*(D+1)/2; ctr ++)
-    {
-       cout <<  array[ctr] << "\t";
-       //<< "[" << i << "," << j << "] -> ctr:"<< ctr 
-       //<< "& idx:" << (i-1 + (j)*(j-1)/2) 
-       
-       i++;
-       if(i>j)
-       {
-           i=1;
-           j++;
-           cout << "\n";
-       }
-       
-
-    }
-    return 0;
+//convert to row-major upper triangular array from matrix
+    for(int i=0; i<dim; i++)
+	     for (int j=i;j<dim; j++)
+	     {
+		    auto idx=(i*dim+j-i*(i+1)/2);
+		    array[idx]=matrix(i,j);
+	     }
+return;
 }
-
-
+    
 
